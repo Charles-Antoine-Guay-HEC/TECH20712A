b0180848159a7d865eeb7205ea6638ed87df054f
Charles-Antoine Guay <caguay@GC7MKD.local>
2025-10-31 13:58:24 -0400

    Question 2 terminé

--- HomeWork/Homework_2.sql
+++ HomeWork/Homework_2.sql
@@ -1,10 +1,11 @@
 USE AdventureWorks2022;
 
+-- Question 1 --
+
 SELECT TOP 500
     CONCAT(p.FirstName, ' ', ISNULL(p.MiddleName, ''), ' ', p.LastName) AS FullName,
     ea.EmailAddress AS Email,
-    CONCAT(a.AddressLine1, ', ', ISNULL(a.AddressLine2, ''), ', ', a.City, ', ', sp.Name, ', ', a.PostalCode) AS Address,
-    p.EmailPromotion
+    CONCAT(a.AddressLine1, ', ', ISNULL(a.AddressLine2, ''), ', ', a.City, ', ', sp.Name, ', ', a.PostalCode) AS Address
 FROM Sales.Customer AS c
 JOIN Person.Person AS p
     ON c.PersonID = p.BusinessEntityID
@@ -15,7 +16,16 @@ JOIN Person.StateProvince AS sp
 JOIN Person.EmailAddress AS ea
     ON p.BusinessEntityID = ea.BusinessEntityID
 WHERE 
-    p.FirstName LIKE '[A-C]%' -- First name starts with A, B, or C
-    AND EmailPromotion > 0 -- Opted in for email promotions
+    p.FirstName LIKE '[A-C]%'   -- First name starts with A, B, or C
+    AND EmailPromotion > 0   -- Opted in for email promotions
 ORDER BY 
     FullName ASC; -- Sort alphabetically by Full Name
+
+-- Question 2 --
+SELECT DISTINCT
+    p.Name AS ProductName,
+    p.Style
+FROM sales.SalesOrderDetail as so
+JOIN Production.Product as p
+    ON so.ProductID = p.ProductID
+WHERE p.Style = 'w' OR p.Style = 'W'
